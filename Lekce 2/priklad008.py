51e4f1dbc7925effc728be205836aa0092a9de70
maro-create <79040886+maro-create@users.noreply.github.com>
2021-03-02 22:38:45 +0100

    Update priklad008.py

--- Lekce 2/priklad008.py
+++ Lekce 2/priklad008.py
@@ -4,7 +4,7 @@ cenaZpravy = 3
 # Zprava ma max. 180 znaku
 if (len(telCislo)) == 9 or (len(telCislo)) == 13:
     textZpravy = input("Zadejte text zprávy, kterou chcete zaslat:")
-    pocetZprav = (len(textZpravy))//4
+    pocetZprav = (len(textZpravy))//180+1
     cenaZpravy += pocetZprav*(3)
     print(f"Cena zprávy je {cenaZpravy} Kč.")
 else:
